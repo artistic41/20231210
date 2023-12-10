417784c01199528864609efceca7455af567af19
artistic software <artisticsoftware41@gmail.com>
2023-12-10 06:40:14 +0000

    ...

--- 1210/winbets.php
+++ 1210/winbets.php
@@ -66,23 +66,23 @@ return [
 		Race 5
 		*/
 		'Win Bets'  =>  [
-			'2' => '20 HKD',
-			'5' => '20 HKD',
+			'2' => '10 HKD',
+			'5' => '10 HKD',
 			'3' => '10 HKD',
 			'10' => '10 HKD',
-			'6' => '10 HKD',
-			'4' => '10 HKD',
 			'8' => '10 HKD',
 		],
-		'Total Bets'  =>  '90 HKD'
+		'Total Bets'  =>  '50 HKD'
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'Win Bets'  =>  [
-			'3' => '20 HKD',
-			'10' => '20 HKD',
+			'1' => '40 HKD',
+			'3' => '30 HKD',
+			'10' => '30 HKD',
+			'7' => '30 HKD',
 			'11' => '10 HKD',
 			'6' => '10 HKD',
 			'14' => '10 HKD',
@@ -91,7 +91,7 @@ return [
 			'4' => '10 HKD',
 			'13' => '10 HKD',
 		],
-		'Total Bets'  =>  '110 HKD'
+		'Total Bets'  =>  '200 HKD'
 	],
 	'7' => [
 		/**
@@ -107,8 +107,9 @@ return [
 			'6' => '-10 HKD',
 			'7' => '-10 HKD',
 			'8' => '-10 HKD',
+			'9' => '-10 HKD',
 		],
-		'Total Bets'  =>  '-90 HKD'
+		'Total Bets'  =>  '-100 HKD'
 	],
 	'8' => [
 		/**
